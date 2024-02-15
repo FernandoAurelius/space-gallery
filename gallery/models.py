ae607bbf8c7bfec84aadd678ee928449012796dd
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-02-15 10:58:05 -0300

    Define dynamic upload for photographs, default image for "not-found" images, add search engine for "gallery" app

--- gallery/models.py
+++ gallery/models.py
@@ -15,10 +15,10 @@ class Photography(models.Model):
     subtitle = models.CharField(max_length=150, null=False, blank=False)
     category = models.CharField(max_length=15, choices=CATEGORY_OPTIONS, default='')
     description = models.TextField(null=False, blank=False)
-    address = models.CharField(max_length=100, null=False, blank=False)
+    address = models.ImageField(upload_to="photographs/%Y/%m", blank=True)
     published = models.BooleanField(default=False)
     release_date = models.DateField(default=datetime.now, blank=False)
     
 
     def __str__(self):
-        return f'Photography: name = {self.name} | address = {self.address}'
+        return self.name
--- gallery/views.py
+++ gallery/views.py
@@ -9,3 +9,13 @@ def index(request):
 def image(request, photo_id):
     photo = get_object_or_404(Photography, pk=photo_id)
     return render(request, 'gallery/image.html', {'photography': photo})
+
+def search(request):
+    photographys = Photography.objects.order_by('-release_date').filter(published=True)
+
+    if "search" in request.GET:
+        name_search = request.GET['search']
+        if name_search:
+            photographys = photographys.filter(name__icontains=name_search)
+
+    return render(request, 'gallery/search.html', {'cards': photographys})
